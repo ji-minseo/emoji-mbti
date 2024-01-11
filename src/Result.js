ce7d9c10c7f12d8738db8b7b012952907c198d66
nyoenghee <nyeonghee.hong@does.kr>
2024-01-11 14:02:33 +0900

    Update 공유하기 기능

--- src/Result.js
+++ src/Result.js
@@ -4,13 +4,45 @@ import useMbtiStore from './Store';
 
 
 function Result() {
-  const {page ,mbtiList,questionList, mbti,resultCon, setResultCon} = useMbtiStore();
-  let MBTI;
+  const {resultCon} = useMbtiStore();
+  const {Kakao} = window;
+  const KAKAOKEY = 'd5a45aa11ca1775f9d9a83c615fa9f65';
 
-  useEffect(()=>{
-    console.log(resultCon)
-  },[resultCon])
 
+  function shareKakao(){
+    Kakao.cleanup();
+    Kakao.init(KAKAOKEY);
+    Kakao.isInitialized();
+
+    Kakao.Share.sendDefault({
+      objectType: "feed",
+      content: {
+        title: '이모지 스티커팩', // 인자값으로 받은 title
+        description: "나의 MBTI의 이모지는 어떤 모습일지이ㅏㄹ머;ㅣㄴ얼;ㅣ", 
+        imageUrl: 'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+        link: {
+          mobileWebUrl: window.location.href, 
+          // webUrl: window.location.href
+        }
+      },
+      buttons: [
+        {
+          title: "테스트하러 가보기",
+          link: {
+            mobileWebUrl: window.location.href,
+            // webUrl: window.location.href
+          }
+        }
+      ]
+    });
+  }
+  
+  
+
+  function urlClip(){
+    navigator.clipboard.writeText(window.location.href);
+    alert("주소가 복사되었습니다.")
+  }
 
 
 	return (
@@ -34,8 +66,8 @@ function Result() {
           <div className='__left'>
             {resultCon.mbti}
             <div className='__mobilebtns'>
-              <button className='noborder'><img alt="weather" className="weather-icon" src={`${process.env.PUBLIC_URL}/kakao.png`} /></button>
-              <button className='noborder'><img alt="weather" className="weather-icon" src={`${process.env.PUBLIC_URL}/link.png`} /></button>
+              <button className='noborder'><img alt="weather" className="weather-icon" src={`${process.env.PUBLIC_URL}/kakao.png`} onClick={()=>shareKakao()}/></button>
+              <button className='noborder' onClick={()=>urlClip()}><img alt="weather" className="weather-icon" src={`${process.env.PUBLIC_URL}/link.png`} /></button>
             </div>
           </div>
           <div className='__right'>
@@ -43,8 +75,8 @@ function Result() {
             {resultCon.content}
             </div>
             <div className='__btns'>
-              <button className='noborder'><img alt="weather" className="weather-icon" src={`${process.env.PUBLIC_URL}/kakao.png`} /></button>
-              <button className='noborder'><img alt="weather" className="weather-icon" src={`${process.env.PUBLIC_URL}/link.png`} /></button>
+              <button className='noborder'><img alt="weather" className="weather-icon" src={`${process.env.PUBLIC_URL}/kakao.png`} onClick={()=>shareKakao()}/></button>
+              <button className='noborder' onClick={()=>urlClip()}><img alt="weather" className="weather-icon" src={`${process.env.PUBLIC_URL}/link.png`} /></button>
               <button className='retry' onClick={()=>{window.location.reload()}}>다시하기</button>
             </div>
           </div>
