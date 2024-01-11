1bc98c5d5066c1cdaf6bb67837714f6e6a0abf5c
ji-minseo <105339059+does-jimin@users.noreply.github.com>
2024-01-11 14:39:13 +0900

    Revert "Update App.js"
    This reverts commit f81e39b5a9b8e7f06b19d2d5db79a2e3ec841915.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import '../src/scss/Style.scss'
 import { useEffect } from 'react';
 import Result from './Result';
+import Test from './Test';
 import Intro from './intro';
 import useMbtiStore from './Store';
 
